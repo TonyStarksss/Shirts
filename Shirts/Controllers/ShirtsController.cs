138f7d584b524d460949ed6bf2f5ab0a1719090d
Jude <judepat63@gmail.com>
2024-07-16 22:51:53 +0800

    ok

---
+++ Shirts/Controllers/ShirtsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shirts.Models;
+
+namespace Shirts.Controllers
+{
+    public class ShirtsController : Controller
+    {
+        public IActionResult Home()
+        {
+
+            var shirt = new Shirt() { Name = "Bench" };
+            return View(shirt);
+
+        }
+    }
+}
